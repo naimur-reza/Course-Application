c81508b5e98d1edee0b25c723c1e82edc2a4631a
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-11 00:40:11 +0600

    Add app, server,.env

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+export const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running!",
+  });
+});
