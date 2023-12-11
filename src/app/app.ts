3c90d7228c3d0eb4d12e780cc98dd75cb0efa790
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-11 18:39:10 +0600

    Updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { userRouter } from "../model/user/user.router";
 
 export const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/users", userRouter);
+// app.use("/api/users", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
