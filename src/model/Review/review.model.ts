ec8b82d92fe0baa71f35b13c4079d17b33e0e759
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-12 01:43:29 +0600

    add ref in model

--- src/model/Review/review.model.ts
+++ src/model/Review/review.model.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 import { IReview } from "./review.interface";
 
 const reviewSchema = new Schema<IReview>({
-  courseId: { type: Schema.Types.ObjectId },
+  courseId: { type: Schema.Types.ObjectId, ref: "Course" },
   rating: { type: Number },
   review: { type: String },
 });
